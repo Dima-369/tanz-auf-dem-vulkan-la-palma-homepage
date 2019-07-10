891fb01b01843392f9adf347c51d946c9be494e3
Dmytro Butemann <dmytro.butemann@gmail.com>
2019-07-10 20:44:17 +0100

    Add workshops site

---
+++ js/init-gallery-workshops.js
@@ -0,0 +1,12 @@
+$('#gallery1').justifiedGallery({
+	rowHeight: 350,
+});
+$('#gallery2').justifiedGallery({
+	rowHeight: 400,
+});
+$('#gallery3').justifiedGallery({
+	rowHeight: 350,
+});
+$('#gallery4').justifiedGallery({
+	rowHeight: 450,
+});
